da0615b87cf2c7acf9b99570db6834063e0a4219
imam hossen <imam.frontend404@gmail.com>
2025-05-07 02:28:21 +0600

    fixingai-

--- app/component/Pagetwo.js
+++ app/component/Pagetwo.js
@@ -3,15 +3,38 @@
 import React, { useEffect, useRef,useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import numbeingss from './image/numbeingss.png';
-import image2 from './image/image2.png';
 import Image from 'next/image';
-import Link from 'next/link';
-import Crxconving from './Crxconving';
-import CryptoTable from './CryptoTable';
+import smslide1 from './image/smslide1.png'
+
 
 gsap.registerPlugin(ScrollTrigger);
 
+const items = [
+  {
+    id: 1,
+    image: smslide1, // Replace with actual image source
+    title: "Automation<br className='lg:block hidden'/> Machine Learning",
+    description: "Talk With Us",
+  },
+  {
+    id: 2,
+    image: smslide1, // Replace with actual image source
+    title: "Al-Powered<br className='lg:block hidden'/>  Chatbots",
+    description: "Talk With Us",
+  },
+  {
+    id: 3,
+    image: smslide1, // Replace with actual image source
+    title: "Data Analytics<br className='lg:block hidden'/>  Deep Insights",
+    description: "Talk With Us",
+  },
+  {
+    id: 4,
+    image: smslide1, // Replace with actual image source
+    title: "Al Strategy<br className='lg:block hidden'/> Pro Consulting",
+    description: "Talk With Us",
+  },
+];
 
 const Pagetwo = () => {
   const wrapperRef = useRef(null);
@@ -167,32 +190,86 @@ const Pagetwo = () => {
 
     
     return (
-        <div id='About' ref={wrapperRef} className='py-[80px] sm:py-[60px] md:py-[90px] lg:py-[120px] xl:py-[150px] 2xl:py-[180px] px-6 sm:px-0'>
+        <div id='About' ref={wrapperRef} className='py-[60px] md:py-[70px] lg:py-[90px] xl:py-[110px] 2xl:py-[140px] px-6 sm:px-0'>
            <div className='container mx-auto'>
-              {/* <Image ref={wrapperRef} src={image2} alt='image2'/> */}
-              <CryptoTable/>
-              <button className="peoexgolsss flex items-center mx-auto gap-[2px] lg:gap-3 cursor-pointer sm:hidden mt-15">
-    VIEW MORE
-    <svg
-      className="w-[12px] sm:w-[13px] md:w-[14px] lg:w-[15px] xl:w-[16px] 2xl:w-[20px]"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1.75 12C1.75 11.5858 2.08579 11.25 2.5 11.25L20.5007 11.25C20.9149 11.25 21.2507 11.5858 21.2507 12C21.2507 12.4142 20.9149 12.75 20.5007 12.75L2.5 12.75C2.08579 12.75 1.75 12.4142 1.75 12Z"
-        fill="#03FC9E"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.0204 5.42388C16.3386 5.1587 16.8115 5.20168 17.0767 5.51988L21.8101 11.1998C22.1964 11.6634 22.1964 12.3367 21.8101 12.8003L17.0767 18.4802C16.8115 18.7984 16.3386 18.8414 16.0204 18.5762C15.7022 18.311 15.6592 17.8381 15.9244 17.5199L20.5244 12L15.9244 6.48018C15.6592 6.16198 15.7022 5.68906 16.0204 5.42388Z"
-        fill="#03FC9E"
-      />
-    </svg>
-  </button>
+              <h2 className='piconsaisasz text-[20px] sm:text-[24px] md:text-[32px] lg:text-[36px] xl:text-[40px] 2xl:text-[48px] pb-[24px] sm:pb-[32px] md:pb-[36px] lg:pb-[40px] xl:pb-[48px] 2xl:pb-[64px]'>Pioneering AI consulting to drive innovation, <br className='lg:block hidden'/> efficiency, and growth for businesses.</h2>
+              <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-[32px]">
+  {items.map((item) => (
+    <div key={item.id} className="">
+      <div className="poicsebsa !rounded-[20px] ">
+        <div className="bg-[#030110] !rounded-t-[20px] !rounded-b-[10px]">
+          <div className="autodesinania relative !rounded-t-[20px] !rounded-b-[10px] px-[14px] sm:px-[15px] md:px-[16px] lg:px-[20px] xl:px-[24px] 2xl:px-[32px] pt-[14px] sm:pt-[15px] md:pt-[16px] lg:pt-[20px] xl:pt-[24px] 2xl:pt-[32px] pb-[32px] sm:pb-[36px] md:pb-[40px] lg:pb-[48px] xl:pb-[64px] 2xl:pb-[83px] flex justify-start gap-[13px] sm:gap-[14px] md:gap-[15px] lg:gap-[16px] xl:gap-[20px] 2xl:gap-[24px] flex-col">
+            <Image className="w-[32px] sm:w-[36px] md:w-[40px] lg:w-[48px] xl:w-[64px] 2xl:w-[68px] shadow-[0px_25px_23px_0px_#00000040] rounded-4xl" src={item.image} alt="loading" />
+            <h2
+              className="autoalaidns text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[20px] 2xl:text-[22px]"
+              dangerouslySetInnerHTML={{ __html: item.title }}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-1/2 absolute -right-[15%] -top-[16%]"
+              viewBox="0 0 380 340"
+              fill="none"
+            >
+              <g opacity="0.81" filter="url(#filter0_f_4_66)">
+                <ellipse cx="190" cy="170" rx="49" ry="29" fill="#FF6035" fill-opacity="0.81" />
+              </g>
+              <defs>
+                <filter
+                  id="filter0_f_4_66"
+                  x="0.600006"
+                  y="0.600006"
+                  width="378.8"
+                  height="338.8"
+                  filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB"
+                >
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                  <feGaussianBlur stdDeviation="70.2" result="effect1_foregroundBlur_4_66" />
+                </filter>
+              </defs>
+            </svg>
+            <svg
+              className="w-1/2 absolute -right-[5%] -bottom-[28%]"
+              viewBox="0 0 282 286"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g opacity="0.6" filter="url(#filter0_f_4_74)">
+                <ellipse
+                  cx="140.929"
+                  cy="142.995"
+                  rx="32.9794"
+                  ry="61.5994"
+                  transform="rotate(-42.6696 140.929 142.995)"
+                  fill="#FF6941"
+                />
+              </g>
+              <defs>
+                <filter
+                  id="filter0_f_4_74"
+                  x="0.639496"
+                  y="0.475586"
+                  width="280.579"
+                  height="285.038"
+                  filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB"
+                >
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                  <feGaussianBlur stdDeviation="46" result="effect1_foregroundBlur_4_74" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+        <div className="talkewa text-[11px] sm:text-[12px] md:text-[13px] lg:text-[14px] xl:text-[15px] 2xl:text-[16px] px-[14px] sm:px-[15px] md:px-[16px] lg:px-[20px] xl:px-[24px] 2xl:px-[32px] py-[11px] sm:py-[12px] md:py-[13px] lg:py-[14px] xl:py-[15px] 2xl:py-[16px]">
+          {item.description}
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
            </div>
         </div>
     );
