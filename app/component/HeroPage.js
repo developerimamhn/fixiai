d6bb123955caae7895ee6fbb9a56b17b1185ed70
Imam Hossen <imam.frontend404@gmail.com>
2025-05-07 19:48:04 +0600

    hfcv

--- app/component/HeroPage.js
+++ app/component/HeroPage.js
@@ -98,7 +98,7 @@ const HeroPage = () => {
                       Free Consultation
                       </button>
                       <div className='flex items-center justify-center'>
-                        <Image className='w-[32px] sm:w-[36px] md:w-[40px] lg:w-[48px] xl:w-[64px] 2xl:w-[85px] mr-[11px] sm:mr-[12px] md:mr-[13px] lg:mr-[14px] xl:mr-[15px] 2xl:mr-[16px]' src={image1}/>
+                        <Image className='w-[32px] sm:w-[36px] md:w-[40px] lg:w-[48px] xl:w-[64px] 2xl:w-[85px] mr-[11px] sm:mr-[12px] md:mr-[13px] lg:mr-[14px] xl:mr-[15px] 2xl:mr-[16px]' src={image1} alt=''/>
                         <svg xmlns="http://www.w3.org/2000/svg" className='w-[12px] sm:w-[13px] md:w-[14px] lg:w-[15px] xl:w-[16px] 2xl:w-[19px]' viewBox="0 0 19 17" fill="none">
                           <path d="M9.5 14.237L13.8649 16.8458C14.6642 17.324 15.6424 16.6172 15.432 15.7233L14.2751 10.8174L18.1351 7.51216C18.8398 6.90932 18.4611 5.76599 17.5356 5.69324L12.4555 5.26709L10.4676 0.631427C10.11 -0.210476 8.88997 -0.210476 8.53237 0.631427L6.54451 5.25669L1.46443 5.68284C0.538871 5.7556 0.160232 6.89892 0.864921 7.50177L4.72494 10.807L3.56799 15.7129C3.35763 16.6068 4.33578 17.3136 5.13513 16.8355L9.5 14.237Z" fill="#FAD921"/>
                         </svg>
